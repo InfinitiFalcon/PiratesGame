30cd85082d242b2aa01a52542821103e95a604ef
InfinitiFalcon <88100491+InfinitiFalcon@users.noreply.github.com>
2021-10-29 17:36:48 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ const Constraint = Matter.Constraint;
 var engine, world, backgroundImg;
 var canvas, angle, tower, ground, cannon;
 var cannonBall;
+var ball 
+var balls = []
 
 function preload() {
   backgroundImg = loadImage("background.gif");
@@ -16,18 +18,17 @@ function setup() {
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-  angleMode(DEGREES)
-  angle = 20
+  
+  angleMode(DEGREES);
+  angle = 15;
 
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
   World.add(world, ground);
 
   tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
   World.add(world, tower);
-
   cannon = new Cannon(180, 110, 130, 100, angle);
   cannonBall = new CannonBall(cannon.x, cannon.y);
-
 }
 
 function draw() {
@@ -36,7 +37,12 @@ function draw() {
 
   Engine.update(engine);
 
+  push();
+  fill("brown");
+  rectMode(CENTER);
   rect(ground.position.x, ground.position.y, width * 2, 1);
+  pop();
+
   push();
   imageMode(CENTER);
   image(towerImage, tower.position.x, tower.position.y, 160, 310);
@@ -45,11 +51,27 @@ function draw() {
   cannon.display();
   cannonBall.display();
 
-  
+  for(var i = 0; i < balls.length; i++){
+    showcannonBall(balls[i],i)
+  }
 }
 
-function keyReleased(){
-  if (keyCode == DOWN_ARROW){
-    cannonBall.shoot()
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    balls[balls.length - 1].shoot();
+  }
+}
+
+function keyPressed(){
+  if (keyCode === DOWN_ARROW){
+    var cannonBall = new CannonBall(cannon.x,cannon.y)
+    balls.push(cannonBall)
+  }
+}
+
+function showcannonBall(ball,i){
+  if (ball){
+    ball.display()
   }
 }
